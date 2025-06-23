f0ca2e255dba5258a49673a54471671868fd95af
Keerthana H <keerthanadas247@gmail.com>
2025-06-23 12:34:35 +0530

    Passing the Json response body using JsonPath class

--- src/test/java/Basic/FirstDemo.java
+++ src/test/java/Basic/FirstDemo.java
@@ -5,7 +5,6 @@ import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-
 import files.payload;
 
 public class FirstDemo {
@@ -13,17 +12,12 @@ public class FirstDemo {
 	public static void main(String[] args)
 	{
 		RestAssured.baseURI="https://rahulshettyacademy.com";
-		String response=given().log().all().queryParam("key", "qaclick123").header("Content-type","application/json")
-		.body(payload.addPlace()).when().post("/maps/api/place/add/json").then().log().all()
-		.assertThat().statusCode(200).body("status", equalTo("OK"))
-		.header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+		String response=given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body(payload.addPlace()).when().post("/maps/api/place/add/json").then().assertThat().statusCode(200)
+		.body("scope",equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 		System.out.println(response);
 		JsonPath js=new JsonPath(response);
 		String placeId=js.getString("place_id");
 		System.out.println(placeId);
-	
-	given().log().all().queryParam("key", "qaclick123").header("Content-type","application/json")
-	.body("").when().put("/maps/api/place/update/json")
-	.then().assertThat().statusCode(200).body("msg",equalsTo("Address successfully updated"))
 	}
 }
