c80ef419bd25d3f743b436d01b24a958ca6c28ab
Keerthana H <keerthanadas247@gmail.com>
2025-06-29 01:09:35 +0530

    Serialization

--- src/test/java/Basic/OAuthTest.java
+++ src/test/java/Basic/OAuthTest.java
@@ -2,11 +2,22 @@ package Basic;
 
 import static io.restassured.RestAssured.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.testng.Assert;
+
 import io.restassured.path.json.JsonPath;
+import pojo.Api;
 import pojo.GetCourse;
+import pojo.Mobile;
+import pojo.WebAutomation;
 public class OAuthTest {
 
 	public static void main(String[] args) {
+		String[] courseTitles= {"Selenium Webdriver Java","Cypress","Protractor"};
 		String response=given()
 		.formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
 		.formParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
@@ -22,6 +33,34 @@ public class OAuthTest {
 		.when().log().all()
 		.get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").as(GetCourse.class);
 		System.out.println("LinkedIn: "+gc.getLinkedIn());
+		List<Mobile> mobile = gc.getCourses().getMobile();
+		for(Mobile m: mobile)
+		{
+			System.out.println("title"+m.getCourseTitle());
+			System.out.println("Price"+m.getPrice());
+		}
+		List<Api> apiCourses= gc.getCourses().getApi();
+		for(int i=0;i<apiCourses.size();i++)
+		{
+			if(apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing"))
+			{
+				System.out.println("Price of SoapUI Webservices testing:"+apiCourses.get(i).getPrice());
+			}
+		}
+		ArrayList<String> arrayList=new ArrayList<>();
+		List<WebAutomation> w = gc.getCourses().getWebAutomation();
+		for(int i=0;i<w.size();i++)
+		{
+			System.out.println(w.get(i).getCourseTitle());
+		}
+		
+		for(int j=0;j<w.size();j++)
+		{
+			arrayList.add(w.get(j).getCourseTitle());
+		}
+		List<String> expectedList= Arrays.asList(courseTitles);
+		Assert.assertTrue(arrayList.equals(expectedList));
+		
 	}
 
 }
